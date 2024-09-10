b6af3592ac66923fd667c47b3c24559644505094
trsoo24 <trsoo24@naver.com>
2024-09-10 17:30:35 +0900

    이벤트 수정 Dto

---
+++ admin/src/main/java/com/example/admin/domain/dto/event/UpdateEventDto.java
@@ -0,0 +1,12 @@
+package com.example.admin.domain.dto.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateEventDto {
+    private String eventName;
+}
