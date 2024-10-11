fd4fd1e3242b74a8a20fd6862da2eb3792ec51c4
trsoo24 <trsoo24@naver.com>
2024-10-11 17:49:43 +0900

    Component 제거

--- admin/src/main/java/com/example/admin/config/filter/CustomFilter.java
+++ admin/src/main/java/com/example/admin/config/filter/CustomFilter.java
@@ -8,14 +8,12 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 import static com.example.admin.exception.enums.MemberErrorCode.INVALID_TOKEN;
 
-//@Component
 @RequiredArgsConstructor
 public class CustomFilter extends OncePerRequestFilter {
     private final JwtTokenProvider jwtTokenProvider;
