8141402e9332d446740de554456816c24c4ca036
trsoo24 <trsoo24@naver.com>
2024-10-17 14:00:36 +0900

    Refactor : 응답 객체 변경

--- admin/src/main/java/com/example/admin/domain/dto/scheduler_status/SchedulerStatusResponse.java
+++ admin/src/main/java/com/example/admin/domain/dto/scheduler_status/SchedulerStatusResponse.java
@@ -5,17 +5,15 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.sql.Timestamp;
-
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class SchedulerStatusResponse {
-    private String dcb = "GDCB";
+    private final String dcb = "GDCB";
     private String scheduler;
     private String server1;
     private String server2;
     private String lastModifier;
-    private Timestamp lastModifiedDate;
+    private String lastModifiedDate;
 }
