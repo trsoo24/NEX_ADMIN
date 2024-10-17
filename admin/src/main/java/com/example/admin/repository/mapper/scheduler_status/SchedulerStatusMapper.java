cd152632e69d101428ae0f89bfde9ed19bb82eab
jeongdonguk <foodsmith96@naver.com>
2024-10-17 13:41:56 +0900

    refactor: 프로세스 기동 현황 조회 API LIST로 수정

--- admin/src/main/java/com/example/admin/repository/mapper/scheduler_status/SchedulerStatusMapper.java
+++ admin/src/main/java/com/example/admin/repository/mapper/scheduler_status/SchedulerStatusMapper.java
@@ -4,8 +4,10 @@ import com.example.admin.domain.dto.scheduler_status.ModifySchedulerStatusReques
 import com.example.admin.domain.dto.scheduler_status.SchedulerStatusResponse;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface SchedulerStatusMapper {
-    SchedulerStatusResponse getSchedulerStatus();
+    List<SchedulerStatusResponse> getSchedulerStatus();
     void updateSchedulerStatus(ModifySchedulerStatusRequest request);
 }
