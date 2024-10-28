5ed34d03e160da4081d45648b4058a233c50bb1e
trsoo24 <trsoo24@naver.com>
2024-10-28 14:22:11 +0900

    Refactor : DTO 에서 DCB 순서 변경

--- admin/src/main/java/com/example/admin/payment_attempt_history/dto/PaymentAttemptHistoryDto.java
+++ admin/src/main/java/com/example/admin/payment_attempt_history/dto/PaymentAttemptHistoryDto.java
@@ -12,19 +12,19 @@ import lombok.NoArgsConstructor;
 public class PaymentAttemptHistoryDto {
     private String ctn;
     private String createDt;
+    private String dcb;
     private String itemName;
     private String amount;
     private String resultCode;
-    private String dcb;
 
     public static PaymentAttemptHistoryDto toDto(PaymentAttemptHistory paymentAttemptHistory) {
         return PaymentAttemptHistoryDto.builder()
                 .ctn(paymentAttemptHistory.getCtn())
                 .createDt(paymentAttemptHistory.getCreateDt())
+                .dcb("GDCB")
                 .itemName(paymentAttemptHistory.getItemName())
                 .amount(paymentAttemptHistory.getAmount())
                 .resultCode(paymentAttemptHistory.getResultCode())
-                .dcb("GDCB")
                 .build();
     }
 }
