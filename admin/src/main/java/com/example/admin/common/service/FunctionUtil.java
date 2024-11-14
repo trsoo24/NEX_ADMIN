a371577555280e62ea735b9a97cf30ec7be7b479
trsoo24 <trsoo24@naver.com>
2024-11-14 18:23:48 +0900

    Fix : page 객체 제거

--- admin/src/main/java/com/example/admin/common/service/FunctionUtil.java
+++ admin/src/main/java/com/example/admin/common/service/FunctionUtil.java
@@ -1,11 +1,6 @@
 package com.example.admin.common.service;
 
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.text.SimpleDateFormat;
@@ -22,21 +17,6 @@ public class FunctionUtil {
 
     @Value("${stats.last.month.try.cnt}")
     private static String monthTryCnt;
-
-    public <T> Page<T> toPage(List<T> list, int page, int pageSize) {
-        Pageable pageable = PageRequest.of(page - 1, pageSize);
-        int start = (int) pageable.getOffset();
-        int end = Math.min((start + pageable.getPageSize()), list.size());
-
-        if (start >= end) {
-            end = list.size();
-            start = (list.size() / pageSize) * pageSize;
-            pageable = PageRequest.of(list.size() / pageSize, pageSize);
-        }
-
-        List<T> ts = list.subList(start, end);
-        return new PageImpl<>(ts, pageable, list.size());
-    }
 
     public String transCtn(String ctn) {
         if (ctn.contains("-")) {
