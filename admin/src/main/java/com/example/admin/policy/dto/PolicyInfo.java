f36aad59ec21dd094563dbe2e0906995ac55e2a4
trsoo24 <trsoo24@naver.com>
2024-11-14 10:40:57 +0900

    Fix : 결제 정책 예약 날짜 명칭 변경

--- admin/src/main/java/com/example/admin/policy/dto/PolicyInfo.java
+++ admin/src/main/java/com/example/admin/policy/dto/PolicyInfo.java
@@ -5,7 +5,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
-import java.util.Date;
 
 @Getter
 @NoArgsConstructor
@@ -14,7 +13,7 @@ public class PolicyInfo {
     private String policyCode; // 결제 정책 이름
     private String flag; // 결제 허용 여부 ( Y / N )
     private String reservationYn; // 변경 예약 여부
-    private LocalDate policyResponseDt; // 변경 예정 날짜
+    private LocalDate policyRsDt; // 변경 예정 날짜
     private LocalDate createDt; // 최초 생성일
     private LocalDate updateDt; // 최종 변경일
 }
