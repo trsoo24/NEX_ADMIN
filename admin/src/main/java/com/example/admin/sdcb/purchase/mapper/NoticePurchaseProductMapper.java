2befc64372c97e6078a2e44a7f738cc8603734ee
trsoo24 <trsoo24@naver.com>
2024-11-14 18:40:00 +0900

    Fix : Page 객체 삭제

--- admin/src/main/java/com/example/admin/sdcb/purchase/mapper/NoticePurchaseProductMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.admin.sdcb.purchase.mapper;
-
-import com.example.admin.sdcb.purchase.dto.NoticePurchaseProduct;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface NoticePurchaseProductMapper {
-    void insertNotiPurchaseProduct(NoticePurchaseProduct noticePurchaseProduct);
-}
--- admin/src/main/java/com/example/admin/sdcb/purchase/mapper/NoticePurchaseTypeMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.admin.sdcb.purchase.mapper;
-
-import com.example.admin.sdcb.purchase.dto.NoticePurchaseType;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface NoticePurchaseTypeMapper {
-    void insertNotiPurchaseType(NoticePurchaseType noticePurchaseType);
-}
