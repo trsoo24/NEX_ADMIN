631664621ea1569ca61ac727dbf9c3f236c0456e
trsoo24 <trsoo24@naver.com>
2024-11-18 18:53:34 +0900

    fix : Altibase 에 맞게 문법 수정

--- admin/src/main/java/com/example/admin/payment_history/mapper/PaymentHistoryMapper.java
+++ admin/src/main/java/com/example/admin/payment_history/mapper/PaymentHistoryMapper.java
@@ -1,6 +1,5 @@
 package com.example.admin.payment_history.mapper;
 
-import com.example.admin.auth.dto.AuthInfo;
 import com.example.admin.payment_history.dto.PaymentHistory;
 import org.apache.ibatis.annotations.Mapper;
 
@@ -9,6 +8,5 @@ import java.util.Map;
 
 @Mapper
 public interface PaymentHistoryMapper {
-    void insertPaymentHistory(AuthInfo authInfo);
     List<PaymentHistory> getPaymentHistoryList(Map<String, Object> map);
 }
