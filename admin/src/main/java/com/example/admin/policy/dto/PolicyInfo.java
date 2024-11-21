fed000594a3646bb944d419d0f873f61f0926d2e
trsoo24 <trsoo24@naver.com>
2024-11-21 13:40:44 +0900

    fix : Altibase 에 맞게 문법 수정

--- admin/src/main/java/com/example/admin/policy/dto/PolicyInfo.java
+++ admin/src/main/java/com/example/admin/policy/dto/PolicyInfo.java
@@ -12,8 +12,5 @@ import java.util.Date;
 public class PolicyInfo {
     private String policyCode; // 결제 정책 이름
     private String flag; // 결제 허용 여부 ( Y / N )
-    private String reservationYn; // 변경 예약 여부
-    private Date policyRsDt; // 변경 예정 날짜
-    private Date createDt; // 최초 생성일
-    private Date updateDt; // 최종 변경일
+    private Date policyAppliedDate; // 최종 변경일자
 }
