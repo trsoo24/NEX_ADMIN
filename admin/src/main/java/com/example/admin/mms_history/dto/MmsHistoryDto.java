bfc6fd8dc041f82ac43352a239ce819d78a8fa86
trsoo24 <trsoo24@naver.com>
2024-12-11 11:20:40 +0900

    Feat : Result Code  분류

--- admin/src/main/java/com/example/admin/mms_history/dto/MmsHistoryDto.java
+++ admin/src/main/java/com/example/admin/mms_history/dto/MmsHistoryDto.java
@@ -33,4 +33,8 @@ public class MmsHistoryDto {
 
         this.ctn = head + star + tail;
     }
+
+    public void setSendResult(String sendResult) {
+        this.sendResult = sendResult;
+    }
 }
--- admin/src/main/java/com/example/admin/mms_history/service/MmsSendHistoryService.java
+++ admin/src/main/java/com/example/admin/mms_history/service/MmsSendHistoryService.java
@@ -1,21 +1,28 @@
 package com.example.admin.mms_history.service;
 
 import com.example.admin.mms_history.dto.MmsHistoryDto;
+import com.example.admin.mms_history.dto.type.SendResultCode;
 import com.example.admin.mms_history.mapper.MmsSendMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class MmsSendHistoryService {
     private final MmsSendMapper mmsSendMapper;
+    private static final Map<String, String> CODE_TO_MESSAGE_MAP =
+            Arrays.stream(SendResultCode.values())
+                    .collect(Collectors.toMap(SendResultCode::getCode, SendResultCode::getMessage));
+
 
     public List<MmsHistoryDto> getMmsSendHistoryList(String ctn, String startDate, String endDate) {
         String trxNo = MDC.get("trxNo");
@@ -30,11 +37,21 @@ public class MmsSendHistoryService {
         List<MmsHistoryDto> mmsHistoryDtoList = mmsSendMapper.selectMmsHistoryList(requestMap);
 
         for (MmsHistoryDto mmsHistoryDto : mmsHistoryDtoList) {
-            mmsHistoryDto.setCtnBlind();
+//            mmsHistoryDto.setCtnBlind();
+            mappingResultCode(mmsHistoryDto);
         }
 
         log.info("[{}] 응답 = 문자 발송 이력 {} 건 조회 완료", trxNo, mmsHistoryDtoList.size());
 
         return mmsHistoryDtoList;
+    }
+
+    private void mappingResultCode(MmsHistoryDto mmsHistoryDto) {
+        if (mmsHistoryDto.getSendResult() != null) {
+            String message = CODE_TO_MESSAGE_MAP.getOrDefault(mmsHistoryDto.getSendResult(), SendResultCode.UnknownError.getMessage());
+            if (message != null) {
+                mmsHistoryDto.setSendResult(message);
+            }
+        }
     }
 }
