c7890c6b550e07a52a6a1e6ea8970280155fa99c
trsoo24 <trsoo24@naver.com>
2024-12-12 13:44:29 +0900

    Fix : 로그 누락 추가

--- admin/src/main/java/com/example/admin/seller/service/SellerStatDailyService.java
+++ admin/src/main/java/com/example/admin/seller/service/SellerStatDailyService.java
@@ -6,6 +6,7 @@ import com.example.admin.seller.dto.SellerDayStat;
 import com.example.admin.seller.mapper.SellerStatsDailyMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -17,12 +18,20 @@ public class SellerStatDailyService {
     private final SellerStatsDailyMapper sellerStatsDailyMapper;
 
     public List<SellerDayStat> getSellerStatsDaily() {
+        String trxNo = MDC.get("trxNo");
+
         String year = FunctionUtil.yearOfYesterday();
         String month = FunctionUtil.monthOfYesterday();
         String day = FunctionUtil.yesterday();
 
+        log.info("[{}] 요청 = {} 일자 판매자 일별 판매 현황 종합", trxNo, year + "-" + month + "-" + day);
+
         GetSellerStatDto dto = new GetSellerStatDto(year, month, day);
 
-        return sellerStatsDailyMapper.getSellerDayStats(dto);
+        List<SellerDayStat> sellerDayStatList = sellerStatsDailyMapper.getSellerDayStats(dto);
+
+        log.info("[{}] 응답 = 판매자 일별 판매 현황 {} 건 종합 완료", trxNo, sellerDayStatList.size());
+
+        return sellerDayStatList;
     }
 }
